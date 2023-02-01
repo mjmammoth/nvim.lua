b5cca1e1c20084ea35297a47ca7c8fb0ecb70dab
Mark Marryatt <mark.marryatt@dentsu.com>
2023-02-01 19:24:43 +0200

    not sure what happened here

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -56,28 +56,28 @@ return require('packer').startup(function(use)
   use("github/copilot.vim")
   use('eandrju/cellular-automaton.nvim')
 
-  use {
-    "mfussenegger/nvim-dap",
-    opt = true,
-    module = { "dap" },
-    requires = {
-      "theHamsta/nvim-dap-virtual-text",
-      "rcarriga/nvim-dap-ui",
-      "mfussenegger/nvim-dap-python",
-      "nvim-telescope/telescope-dap.nvim",
-      { "jbyuki/one-small-step-for-vimkind", module = "osv" },
-      { "mxsdev/nvim-dap-vscode-js" },
-      {
-        "microsoft/vscode-js-debug",
-        opt = true,
-        run = "npm install --legacy-peer-deps && npm run compile",
-      },
-    },
-    -- config = function()
-    --   require("config.dap").setup()
-    -- end,
-    disable = false,
-  }
+--  use {
+--    "mfussenegger/nvim-dap",
+--    opt = true,
+--    module = { "dap" },
+--    requires = {
+--      "theHamsta/nvim-dap-virtual-text",
+--      "rcarriga/nvim-dap-ui",
+--      "mfussenegger/nvim-dap-python",
+--      "nvim-telescope/telescope-dap.nvim",
+--      { "jbyuki/one-small-step-for-vimkind", module = "osv" },
+--      { "mxsdev/nvim-dap-vscode-js" },
+--      {
+--        "microsoft/vscode-js-debug",
+--        opt = true,
+--        run = "npm install --legacy-peer-deps && npm run compile",
+--      },
+--    },
+--    -- config = function()
+--    --   require("config.dap").setup()
+--    -- end,
+--    disable = false,
+--  }
 
 end)
 
